7b57645d5aa6ee5eb948cd66b9e7c1d7f96c6c32
GitHub Action <action@github.com>
2021-10-07 01:54:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/samcrow/intellijuavcan/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/samcrow/intellijuavcan/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.samcrow.intellijuavcan.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.samcrow.intellijuavcan.MyBundle
 
 class MyApplicationService {
 
